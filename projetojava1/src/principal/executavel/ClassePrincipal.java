0b9e6a8a005f82858a1ca91862fdd4fd367bd215
vandorocha <vanderlandio.zr@gmail.com>
2025-08-30 21:40:54 -0300

    Criado projeto

--- projetojava1/src/principal/executavel/ClassePrincipal.java
+++ projetojava1/src/principal/executavel/ClassePrincipal.java
@@ -3,7 +3,7 @@ package principal.executavel;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Primeiro programa");
 
 	}
 
