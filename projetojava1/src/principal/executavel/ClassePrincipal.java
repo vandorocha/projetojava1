b8508c9d016209e08342c38f823a85643d0b90b8
vandorocha <vanderlandio.zr@gmail.com>
2025-08-30 21:44:30 -0300

    Criado

--- projetojava1/src/principal/executavel/ClassePrincipal.java
+++ projetojava1/src/principal/executavel/ClassePrincipal.java
@@ -3,7 +3,7 @@ package principal.executavel;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		System.out.println("Primeiro programa");
+		System.out.println("Primeiro programa Java");
 
 	}
 
